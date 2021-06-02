8a6715fe0cc58597a1ef0fd09191b7a1747034ba
Fadhilah24 <62755456+Fadhilah24@users.noreply.github.com>
2021-06-01 18:36:35 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,10 @@
 |
 */
 
-$router->group(['prefix'=>'api/beta'], function () use ($router) {
-    //return $router->app->version();
+
 	$router->get('/kolam', 'RegistrasikolamController@index');
     $router->post('/kolam', 'RegistrasikolamController@create');
     $router->get('/kolam/{id}', 'RegistrasikolamController@show');
     $router->put('/kolam/{id}', 'RegistrasikolamController@update');
     $router->delete('/kolam/{id}', 'RegistrasikolamController@destroy');
 	
-});
